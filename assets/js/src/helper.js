e1df653cd38712ff672c93e954cb11bb5219b568
Shekhar Patnaik <patnaikshekhar@gmail.com>
2016-02-09 20:10:24 +0000

    Fixed Export shim

--- assets/js/src/helper.js
+++ assets/js/src/helper.js
@@ -1,4 +1,7 @@
 var ipcRenderer = require('electron').ipcRenderer;
+var remote = require('remote'); 
+var dialog = remote.require('dialog');
+var fs = require('fs');
 
 var Helper = {
     openWindow: function(url, username, password) {
@@ -6,6 +9,17 @@ var Helper = {
             username: username,
             password: password,
             url: url
+        });
+    },
+    
+    download: function(json) {
+        var options = { filters: [
+            { name: 'export', extensions: ['json'] }
+        ]};
+        
+        dialog.showSaveDialog(options, function (fileName) {
+            if (fileName === undefined) return;
+            fs.writeFile(fileName, JSON.stringify(json), function (err) {});
         });
     }
 };
